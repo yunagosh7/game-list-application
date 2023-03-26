f5707febd78694c06b8897af721182e07587947f
Vila Juan Cruz <yunagosh7@gmail.com>
2023-03-25 23:15:20 -0300

    Initial commit

---
+++ app/src/main/java/com/example/gamelist/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gamelist.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gamelist.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
